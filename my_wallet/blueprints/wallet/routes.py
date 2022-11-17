f7f262e9936c5e4c85c03921051107a75f3cdb53
Ilya Lebedev <melevir@gmail.com>
2022-11-17 18:31:17 +0400

    add wireframe for public api

--- my_wallet/blueprints/wallet/routes.py
+++ my_wallet/blueprints/wallet/routes.py
@@ -1,7 +1,8 @@
 from flask import Blueprint
 
-from my_wallet.blueprints.wallet.views import wallets_list, wallet_detail, transaction_delete, transaction_add, \
+from my_wallet.blueprints.wallet.views.private import wallets_list, wallet_detail, transaction_delete, transaction_add,\
     wallet_add, wallet_delete, wallet_access, wallet_access_remove
+from my_wallet.blueprints.wallet.views.public import api_wallets, api_wallet, api_transactions, api_transaction
 
 
 def configure_routes(blueprint: Blueprint) -> None:
@@ -18,3 +19,17 @@ def configure_routes(blueprint: Blueprint) -> None:
 
     blueprint.add_url_rule("/<int:wallet_id>/transaction/add", view_func=transaction_add, methods=["GET", "POST"])
     blueprint.add_url_rule("/transaction/<int:transaction_id>/delete", view_func=transaction_delete, methods=["POST"])
+
+    # public API
+    blueprint.add_url_rule("/api/wallets", view_func=api_wallets, methods=["GET", "POST"])
+    blueprint.add_url_rule("/api/wallet/<int:wallet_id>", view_func=api_wallet, methods=["GET", "PUT", "DELETE"])
+    blueprint.add_url_rule(
+        "/api/wallet/<int:wallet_id>/transactions",
+        view_func=api_transactions,
+        methods=["GET", "POST"],
+    )
+    blueprint.add_url_rule(
+        "/api/wallet/<int:wallet_id>/transaction/<int:transaction_id>",
+        view_func=api_transaction,
+        methods=["GET", "PUT", "DELETE"],
+    )
---
+++ my_wallet/blueprints/wallet/views/public.py
@@ -0,0 +1,16 @@
+
+
+def api_wallets():
+    pass
+
+
+def api_wallet(wallet_id):
+    pass
+
+
+def api_transactions(wallet_id):
+    pass
+
+
+def api_transaction(wallet_id, transaction_id):
+    pass
