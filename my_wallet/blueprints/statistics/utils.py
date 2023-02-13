fe0f096bab23c6c5ae1bc2ba75b919bc189f8cac
Mikhail Murashev <mihail.mmm@inbox.ru>
2023-02-13 23:26:30 +0300

    Добавлены тесты к ф-циям get_connection_dsn и is_report_range_valid

--- my_wallet/blueprints/statistics/utils.py
+++ my_wallet/blueprints/statistics/utils.py
@@ -3,3 +3,9 @@ import datetime
 
 def is_report_range_valid(date_from: datetime.date, date_to: datetime.date) -> bool:
     return date_to <= date_from
+
+
+if __name__ == '__main__':
+    date_from = datetime.date(2023, 2, 1)
+    date_to = datetime.date(2023, 1, 1)
+    assert is_report_range_valid(date_from, date_to)
