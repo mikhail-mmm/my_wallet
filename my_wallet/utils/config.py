c82858b6633193f25f5e122bcb65069c18366c4f
Mikhail <81187343+mikhail-mmm@users.noreply.github.com>
2023-02-25 16:22:05 +0300

    Добавлены тесты к ф-циям get_connection_dsn и is_report_range_valid (#1)
    * Добавлены тесты к ф-циям get_connection_dsn и is_report_range_valid
    
    * Добавлена проверка с помощью pytest

--- my_wallet/utils/config.py
+++ my_wallet/utils/config.py
@@ -6,3 +6,15 @@ def get_connection_dsn(config: Mapping[str, Any]) -> str:
         f"postgresql://{config['POSTGRES_USER']}:{config['POSTGRES_PASSWORD']}@"
         f"{config['POSTGRES_HOST']}:{config['POSTGRES_PORT']}/{config['POSTGRES_DBNAME']}"
     )
+
+
+def test_get_connection_dsn():
+    config = {
+        'POSTGRES_USER': 'user',
+        'POSTGRES_PASSWORD': 'password',
+        'POSTGRES_HOST': 'localhost',
+        'POSTGRES_PORT': '123',
+        'POSTGRES_DBNAME': 'test'
+    }
+    result_dsn = get_connection_dsn(config)
+    assert result_dsn == 'postgresql://user:password@localhost:123/test'
